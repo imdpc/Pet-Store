8d8ae2d673237bc6acaf5e8f51cffa4380334bac
Prasad Adhav <prasadadhav22@gmail.com>
2022-07-27 13:10:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import "./App.css";
-import Home from "./Components/Home/Home";
 import FeaturedIn from "./Containers/FeaturedIn/FeaturedIn";
-import HomeScreen from "./Containers/HomeScreen.css/HomeScreen";
 import TargetSale from "./Containers/TargetSales/TargetSale";
+import HomeShopNow from "./Components/HomeShopNow/HomeShopNow";
+import NavBar from "./Components/NavBar/NavBar";
+import ExploreWorld from "./Containers/ExploreWorld/ExploreWorld";
+import UkStore from "./Components/UkStore/UkStore";
+import ProductNav from "./Components/ProductNav/ProductNav";
 
 function App() {
   return (
     <>
-      {/* <Home />
-      <HomeScreen /> */}
+      <NavBar />
+      <HomeShopNow />
       <TargetSale />
       <FeaturedIn />
+      <ExploreWorld />
+      <UkStore />
+      <ProductNav />
     </>
   );
 }
