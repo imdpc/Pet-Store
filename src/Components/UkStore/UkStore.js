b9713f4f19ef2cd1183d90927563d5fbca176977
Prasad Adhav <prasadadhav22@gmail.com>
2022-07-27 18:11:45 +0530

    Created Explore More Page

--- src/Components/UkStore/UkStore.js
+++ src/Components/UkStore/UkStore.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import './UkStore.css'
-import ukimage from '../../Assets/pet-shop-img.png'
+import React from "react";
+import "./UkStore.css";
+import ukimage from "../../Assets/pet-shop-img.png";
 
 const UkStore = () => {
   return (
     <>
-      <div style={{ display: 'flex', columnGap: '85px' }}>
+      <div style={{ display: "flex", columnGap: "85px" }}>
         <div>
           <img src={ukimage} alt="" className="uk-image" />
         </div>
@@ -19,7 +19,7 @@ const UkStore = () => {
             Products include foods formulated for adult,working dogs <br />
             and puppies,treats and rewards. The UK's third largest <br />
             brand by value. Wagg Treats the 4th largest and fastest <br />
-            growing treat brand in the UK.{' '}
+            growing treat brand in the UK.{" "}
           </p>
           <br />
           <div>
@@ -30,7 +30,7 @@ const UkStore = () => {
                   src="https://cdn-icons-png.flaticon.com/512/6459/6459980.png"
                   alt=""
                 />
-                {'   '}
+                {"   "}
                 <p>Luxurious Skincare Solution</p>
               </li>
               <li className="uk-store-key-points">
@@ -39,7 +39,7 @@ const UkStore = () => {
                   src="https://cdn-icons-png.flaticon.com/512/6459/6459980.png"
                   alt=""
                 />
-                {'   '}
+                {"   "}
                 Educating and Empowering
               </li>
               <li className="uk-store-key-points">
@@ -48,17 +48,19 @@ const UkStore = () => {
                   src="https://cdn-icons-png.flaticon.com/512/6459/6459980.png"
                   alt=""
                 />
-                {'   '}
+                {"   "}
                 Positive and Transperent Industry.
               </li>
             </ul>
           </div>
           <br />
-          <button className="uk-store-btn">Shop Now</button>
+          <button type="submit" className="uk-store-btn">
+            Shop Now
+          </button>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default UkStore
+export default UkStore;
