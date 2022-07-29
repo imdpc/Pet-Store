c8efc1bf920091298228bf024e063dc9589a1711
Imakashkendre <akash.shopisthan@gmail.com>
2022-07-29 13:46:37 +0530

    explore more and shopinsta changes

--- src/Containers/ShopOurInstagram/ShopOurInstagram.js
+++ src/Containers/ShopOurInstagram/ShopOurInstagram.js
@@ -5,60 +5,62 @@ const ShopOurInstagram = () => {
   return (
     <>
       <div className="main-container-insta-cards">
-        <div className="heading-section-shop-our-insta-and-waggpetfood">
-          <div className="heading-tag-for-shop-our-instgram">
-            <div className="header-text-insta-shop">
-              <p>Shop our Instagram</p>
+        <div className="wid-for-cards">
+          <div className="heading-section-shop-our-insta-and-waggpetfood">
+            <div className="heading-tag-for-shop-our-instgram">
+              <div className="header-text-insta-shop">
+                <p>Shop our Instagram</p>
+              </div>
+              <div className="follow-us-text-insta-shop">
+                <p>Follow us on Instagram for a daily dose of magic.</p>
+              </div>
             </div>
-            <div className="follow-us-text-insta-shop">
-              <p>Follow us on Instagram for a daily dose of magic.</p>
+            <div className="waggpet-food-text-and-image">
+              <div className="image-contain-arrow-right">
+                <img
+                  src="https://cdn-icons.flaticon.com/png/128/5611/premium/5611889.png?token=exp=1659011817~hmac=b5f39d32bc5731370969af0f087f802d"
+                  alt=""
+                  srcset=""
+                />
+              </div>
+              <div className="wag-pet-food-sub-heading">
+                <p>Waggpetfood</p>
+              </div>
             </div>
           </div>
-          <div className="waggpet-food-text-and-image">
-            <div className="image-contain-arrow-right">
+          <div className="contain-all-insta-images">
+            <div className="card-for-insta-image-1">
               <img
-                src="https://cdn-icons.flaticon.com/png/128/5611/premium/5611889.png?token=exp=1659011817~hmac=b5f39d32bc5731370969af0f087f802d"
+                src="https://i.pinimg.com/originals/29/ac/4b/29ac4bfe86592796754892a453ab6b79.jpg"
                 alt=""
-                srcset=""
+              />
+              {/* <div className="overlay">
+                <a href="http://" className="icon">
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/128/1077/1077042.png"
+
+                  />
+                </a>
+              </div> */}
+            </div>
+            <div className="card-for-insta-image-2">
+              <img
+                src="https://i.pinimg.com/originals/29/ac/4b/29ac4bfe86592796754892a453ab6b79.jpg"
+                alt=""
               />
             </div>
-            <div className="wag-pet-food-sub-heading">
-              <p>Waggpetfood</p>
+            <div className="card-for-insta-image-3">
+              <img
+                src="https://i.pinimg.com/originals/29/ac/4b/29ac4bfe86592796754892a453ab6b79.jpg"
+                alt=""
+              />
             </div>
-          </div>
-        </div>
-        <div className="contain-all-insta-images">
-          <div className="card-for-insta-image-1">
-            <img
-              src="https://i.pinimg.com/originals/29/ac/4b/29ac4bfe86592796754892a453ab6b79.jpg"
-              alt=""
-            />
-            <div className="overlay">
-              <a href="http://" className="icon">
-                <img
-                  src="https://cdn-icons-png.flaticon.com/128/1077/1077042.png"
-                  
-                />
-              </a>
+            <div className="card-for-insta-image-4">
+              <img
+                src="https://i.pinimg.com/originals/29/ac/4b/29ac4bfe86592796754892a453ab6b79.jpg"
+                alt=""
+              />
             </div>
-          </div>
-          <div className="card-for-insta-image-2">
-            <img
-              src="https://i.pinimg.com/originals/29/ac/4b/29ac4bfe86592796754892a453ab6b79.jpg"
-              alt=""
-            />
-          </div>
-          <div className="card-for-insta-image-3">
-            <img
-              src="https://i.pinimg.com/originals/29/ac/4b/29ac4bfe86592796754892a453ab6b79.jpg"
-              alt=""
-            />
-          </div>
-          <div className="card-for-insta-image-4">
-            <img
-              src="https://i.pinimg.com/originals/29/ac/4b/29ac4bfe86592796754892a453ab6b79.jpg"
-              alt=""
-            />
           </div>
         </div>
       </div>
