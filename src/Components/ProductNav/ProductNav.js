349a0b6627942cdf246845ed5ad675d7ffcf8079
Prasad Adhav <prasadadhav22@gmail.com>
2022-07-29 15:27:37 +0530

    Created Product Component

--- src/Components/ProductNav/ProductNav.js
+++ src/Components/ProductNav/ProductNav.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Product from "../Product/Product";
 import "./ProductNav.css";
 
 const ProductNav = () => {
@@ -14,7 +15,8 @@ const ProductNav = () => {
                   fontFamily: "Beau Rivage",
                 }}
               >
-                Our Best Selling<br />
+                Our Best Selling
+                <br />
                 Products
               </h3>
               <div className="product-navbar-all-content">
@@ -27,218 +29,10 @@ const ProductNav = () => {
             </div>
           </div>
           <div className="all-cards">
-            <div className="shopping-product-cards-container">
-              <div className="product-info-card-for-selection">
-                <div className="image-for-like-the-product">
-                  <img
-                    style={{
-                      height: "13px",
-                      width: "13px",
-                    }}
-                    src="https://cdn-icons-png.flaticon.com/128/1077/1077035.png"
-                    alt=""
-                    srcset=""
-                  />
-                </div>
-                <div className="image-of-the-product-details">
-                  <img
-                    style={{
-                      height: "190px",
-                      width: "146px",
-                    }}
-                    src="https://waggfoods.com/assets/images/products/_productImage1xWebp/137/wagg_tasty_bones_150g.webp"
-                    alt=""
-                    srcset=""
-                  />
-                </div>
-              </div>
-              <div className="bottom-section-about-products-details">
-                <div className="product-name-for-selection">
-                  <p>Tasty Bones</p>
-                </div>
-                <div className="sizes-for-all-dogs-selection">
-                  <p>All Dog Size</p>
-                </div>
-                <div className="review-price-and-add-to-card-button">
-                  <p>
-                    <span>$60.00</span>
-                    <span className="star-review-for-product">
-                      &#11088;{" "}
-                    </span> 4.9{" "}
-                    <span style={{ color: "gray" }}>(8000 reviews)</span>
-                  </p>
-                  <div className="container-image-for-add-to-cart">
-                    <button className="image-for-add-to-cart" type="submit">
-                      <img
-                        style={{ height: "29px", width: "25px" }}
-                        src="https://cdn-icons-png.flaticon.com/128/4329/4329263.png"
-                        alt=""
-                        srcset=""
-                      />
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="shopping-product-cards-container">
-              <div className="product-info-card-for-selection">
-                <div className="image-for-like-the-product">
-                  <img
-                    style={{
-                      height: "13px",
-                      width: "13px",
-                    }}
-                    src="https://cdn-icons-png.flaticon.com/128/1077/1077035.png"
-                    alt=""
-                    srcset=""
-                  />
-                </div>
-                <div className="image-of-the-product-details">
-                  <img
-                    style={{
-                      height: "190px",
-                      width: "146px",
-                    }}
-                    src="https://waggfoods.com/assets/images/products/_productImage1xWebp/137/wagg_tasty_bones_150g.webp"
-                    alt=""
-                    srcset=""
-                  />
-                </div>
-              </div>
-              <div className="bottom-section-about-products-details">
-                <div className="product-name-for-selection">
-                  <p>Tasty Bones</p>
-                </div>
-                <div className="sizes-for-all-dogs-selection">
-                  <p>All Dog Size</p>
-                </div>
-                <div className="review-price-and-add-to-card-button">
-                  <p>
-                    <span>$60.00</span>
-                    <span className="star-review-for-product">
-                      &#11088;{" "}
-                    </span> 4.9{" "}
-                    <span style={{ color: "gray" }}>(8000 reviews)</span>
-                  </p>
-                  <div className="container-image-for-add-to-cart">
-                    <button className="image-for-add-to-cart" type="submit">
-                      <img
-                        style={{ height: "29px", width: "25px" }}
-                        src="https://cdn-icons-png.flaticon.com/128/4329/4329263.png"
-                        alt=""
-                        srcset=""
-                      />
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="shopping-product-cards-container">
-              <div className="product-info-card-for-selection">
-                <div className="image-for-like-the-product">
-                  <img
-                    style={{
-                      height: "13px",
-                      width: "13px",
-                    }}
-                    src="https://cdn-icons-png.flaticon.com/128/1077/1077035.png"
-                    alt=""
-                    srcset=""
-                  />
-                </div>
-                <div className="image-of-the-product-details">
-                  <img
-                    style={{
-                      height: "190px",
-                      width: "146px",
-                    }}
-                    src="https://waggfoods.com/assets/images/products/_productImage1xWebp/137/wagg_tasty_bones_150g.webp"
-                    alt=""
-                    srcset=""
-                  />
-                </div>
-              </div>
-              <div className="bottom-section-about-products-details">
-                <div className="product-name-for-selection">
-                  <p>Tasty Bones</p>
-                </div>
-                <div className="sizes-for-all-dogs-selection">
-                  <p>All Dog Size</p>
-                </div>
-                <div className="review-price-and-add-to-card-button">
-                  <p>
-                    <span>$60.00</span>
-                    <span className="star-review-for-product">
-                      &#11088;{" "}
-                    </span> 4.9{" "}
-                    <span style={{ color: "gray" }}>(8000 reviews)</span>
-                  </p>
-                  <div className="container-image-for-add-to-cart">
-                    <button className="image-for-add-to-cart" type="submit">
-                      <img
-                        style={{ height: "29px", width: "25px" }}
-                        src="https://cdn-icons-png.flaticon.com/128/4329/4329263.png"
-                        alt=""
-                        srcset=""
-                      />
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="shopping-product-cards-container">
-              <div className="product-info-card-for-selection">
-                <div className="image-for-like-the-product">
-                  <img
-                    style={{
-                      height: "13px",
-                      width: "13px",
-                    }}
-                    src="https://cdn-icons-png.flaticon.com/128/1077/1077035.png"
-                    alt=""
-                    srcset=""
-                  />
-                </div>
-                <div className="image-of-the-product-details">
-                  <img
-                    style={{
-                      height: "190px",
-                      width: "146px",
-                    }}
-                    src="https://waggfoods.com/assets/images/products/_productImage1xWebp/137/wagg_tasty_bones_150g.webp"
-                    alt=""
-                    srcset=""
-                  />
-                </div>
-              </div>
-              <div className="bottom-section-about-products-details">
-                <div className="product-name-for-selection">
-                  <p>Tasty Bones</p>
-                </div>
-                <div className="sizes-for-all-dogs-selection">
-                  <p>All Dog Size</p>
-                </div>
-                <div className="review-price-and-add-to-card-button">
-                  <p>
-                    <span>$60.00</span>
-                    <span className="star-review-for-product">
-                      &#11088;{" "}
-                    </span> 4.9{" "}
-                    <span style={{ color: "gray" }}>(8000 reviews)</span>
-                  </p>
-                  <div className="container-image-for-add-to-cart">
-                    <button className="image-for-add-to-cart" type="submit">
-                      <img
-                        style={{ height: "29px", width: "25px" }}
-                        src="https://cdn-icons-png.flaticon.com/128/4329/4329263.png"
-                        alt=""
-                        srcset=""
-                      />
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <Product />
+            <Product />
+            <Product />
+            <Product />
           </div>
         </div>
       </div>
