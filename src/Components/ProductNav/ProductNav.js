abfd23d69b852051fcf57e172f46bd2958ac08e0
Imakashkendre <akash.shopisthan@gmail.com>
2022-07-29 12:37:44 +0530

    changes

--- src/Components/ProductNav/ProductNav.js
+++ src/Components/ProductNav/ProductNav.js
@@ -5,235 +5,237 @@ const ProductNav = () => {
   return (
     <>
       <div className="flex-main-container-shopping-products-and-navbar">
-        <div style={{ gap: "50px" }}>
-          <div className="product-navbar-heading">
-            <h3
-              style={{
-                fontSize: "40px",
-                fontFamily: "Beau Rivage",
-              }}
-            >
-              Our Best Selling <br />
-              Products
-            </h3>
-            <div className="product-navbar-all-content">
-              <p className="product-navbar-sub-content">All product</p>
-              <p className="product-navbar-sub-content">Dog Treats</p>
-              <p className="product-navbar-sub-content">Dog Food</p>
-              <p className="product-navbar-sub-content">Special Diets</p>
-              <p className="product-navbar-sub-content">Puppy Food</p>
+        <div className="wid-product-nav">
+          <div>
+            <div className="product-navbar-heading">
+              <h3
+                style={{
+                  fontSize: "40px",
+                  fontFamily: "Beau Rivage",
+                }}
+              >
+                Our Best Selling<br />
+                Products
+              </h3>
+              <div className="product-navbar-all-content">
+                <p className="product-navbar-sub-content">All product</p>
+                <p className="product-navbar-sub-content">Dog Treats</p>
+                <p className="product-navbar-sub-content">Dog Food</p>
+                <p className="product-navbar-sub-content">Special Diets</p>
+                <p className="product-navbar-sub-content">Puppy Food</p>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="all-cards">
-          <div className="shopping-product-cards-container">
-            <div className="product-info-card-for-selection">
-              <div className="image-for-like-the-product">
-                <img
-                  style={{
-                    height: "13px",
-                    width: "13px",
-                  }}
-                  src="https://cdn-icons-png.flaticon.com/128/1077/1077035.png"
-                  alt=""
-                  srcset=""
-                />
+          <div className="all-cards">
+            <div className="shopping-product-cards-container">
+              <div className="product-info-card-for-selection">
+                <div className="image-for-like-the-product">
+                  <img
+                    style={{
+                      height: "13px",
+                      width: "13px",
+                    }}
+                    src="https://cdn-icons-png.flaticon.com/128/1077/1077035.png"
+                    alt=""
+                    srcset=""
+                  />
+                </div>
+                <div className="image-of-the-product-details">
+                  <img
+                    style={{
+                      height: "190px",
+                      width: "146px",
+                    }}
+                    src="https://waggfoods.com/assets/images/products/_productImage1xWebp/137/wagg_tasty_bones_150g.webp"
+                    alt=""
+                    srcset=""
+                  />
+                </div>
               </div>
-              <div className="image-of-the-product-details">
-                <img
-                  style={{
-                    height: "190px",
-                    width: "146px",
-                  }}
-                  src="https://waggfoods.com/assets/images/products/_productImage1xWebp/137/wagg_tasty_bones_150g.webp"
-                  alt=""
-                  srcset=""
-                />
-              </div>
-            </div>
-            <div className="bottom-section-about-products-details">
-              <div className="product-name-for-selection">
-                <p>Tasty Bones</p>
-              </div>
-              <div className="sizes-for-all-dogs-selection">
-                <p>All Dog Size</p>
-              </div>
-              <div className="review-price-and-add-to-card-button">
-                <p>
-                  <span>$60.00</span>
-                  <span className="star-review-for-product">
-                    &#11088;{" "}
-                  </span> 4.9{" "}
-                  <span style={{ color: "gray" }}>(8000 reviews)</span>
-                </p>
-                <div className="container-image-for-add-to-cart">
-                  <button className="image-for-add-to-cart" type="submit">
-                    <img
-                      style={{ height: "29px", width: "25px" }}
-                      src="https://cdn-icons-png.flaticon.com/128/4329/4329263.png"
-                      alt=""
-                      srcset=""
-                    />
-                  </button>
+              <div className="bottom-section-about-products-details">
+                <div className="product-name-for-selection">
+                  <p>Tasty Bones</p>
+                </div>
+                <div className="sizes-for-all-dogs-selection">
+                  <p>All Dog Size</p>
+                </div>
+                <div className="review-price-and-add-to-card-button">
+                  <p>
+                    <span>$60.00</span>
+                    <span className="star-review-for-product">
+                      &#11088;{" "}
+                    </span> 4.9{" "}
+                    <span style={{ color: "gray" }}>(8000 reviews)</span>
+                  </p>
+                  <div className="container-image-for-add-to-cart">
+                    <button className="image-for-add-to-cart" type="submit">
+                      <img
+                        style={{ height: "29px", width: "25px" }}
+                        src="https://cdn-icons-png.flaticon.com/128/4329/4329263.png"
+                        alt=""
+                        srcset=""
+                      />
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="shopping-product-cards-container">
-            <div className="product-info-card-for-selection">
-              <div className="image-for-like-the-product">
-                <img
-                  style={{
-                    height: "13px",
-                    width: "13px",
-                  }}
-                  src="https://cdn-icons-png.flaticon.com/128/1077/1077035.png"
-                  alt=""
-                  srcset=""
-                />
+            <div className="shopping-product-cards-container">
+              <div className="product-info-card-for-selection">
+                <div className="image-for-like-the-product">
+                  <img
+                    style={{
+                      height: "13px",
+                      width: "13px",
+                    }}
+                    src="https://cdn-icons-png.flaticon.com/128/1077/1077035.png"
+                    alt=""
+                    srcset=""
+                  />
+                </div>
+                <div className="image-of-the-product-details">
+                  <img
+                    style={{
+                      height: "190px",
+                      width: "146px",
+                    }}
+                    src="https://waggfoods.com/assets/images/products/_productImage1xWebp/137/wagg_tasty_bones_150g.webp"
+                    alt=""
+                    srcset=""
+                  />
+                </div>
               </div>
-              <div className="image-of-the-product-details">
-                <img
-                  style={{
-                    height: "190px",
-                    width: "146px",
-                  }}
-                  src="https://waggfoods.com/assets/images/products/_productImage1xWebp/137/wagg_tasty_bones_150g.webp"
-                  alt=""
-                  srcset=""
-                />
-              </div>
-            </div>
-            <div className="bottom-section-about-products-details">
-              <div className="product-name-for-selection">
-                <p>Tasty Bones</p>
-              </div>
-              <div className="sizes-for-all-dogs-selection">
-                <p>All Dog Size</p>
-              </div>
-              <div className="review-price-and-add-to-card-button">
-                <p>
-                  <span>$60.00</span>
-                  <span className="star-review-for-product">
-                    &#11088;{" "}
-                  </span> 4.9{" "}
-                  <span style={{ color: "gray" }}>(8000 reviews)</span>
-                </p>
-                <div className="container-image-for-add-to-cart">
-                  <button className="image-for-add-to-cart" type="submit">
-                    <img
-                      style={{ height: "29px", width: "25px" }}
-                      src="https://cdn-icons-png.flaticon.com/128/4329/4329263.png"
-                      alt=""
-                      srcset=""
-                    />
-                  </button>
+              <div className="bottom-section-about-products-details">
+                <div className="product-name-for-selection">
+                  <p>Tasty Bones</p>
+                </div>
+                <div className="sizes-for-all-dogs-selection">
+                  <p>All Dog Size</p>
+                </div>
+                <div className="review-price-and-add-to-card-button">
+                  <p>
+                    <span>$60.00</span>
+                    <span className="star-review-for-product">
+                      &#11088;{" "}
+                    </span> 4.9{" "}
+                    <span style={{ color: "gray" }}>(8000 reviews)</span>
+                  </p>
+                  <div className="container-image-for-add-to-cart">
+                    <button className="image-for-add-to-cart" type="submit">
+                      <img
+                        style={{ height: "29px", width: "25px" }}
+                        src="https://cdn-icons-png.flaticon.com/128/4329/4329263.png"
+                        alt=""
+                        srcset=""
+                      />
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="shopping-product-cards-container">
-            <div className="product-info-card-for-selection">
-              <div className="image-for-like-the-product">
-                <img
-                  style={{
-                    height: "13px",
-                    width: "13px",
-                  }}
-                  src="https://cdn-icons-png.flaticon.com/128/1077/1077035.png"
-                  alt=""
-                  srcset=""
-                />
+            <div className="shopping-product-cards-container">
+              <div className="product-info-card-for-selection">
+                <div className="image-for-like-the-product">
+                  <img
+                    style={{
+                      height: "13px",
+                      width: "13px",
+                    }}
+                    src="https://cdn-icons-png.flaticon.com/128/1077/1077035.png"
+                    alt=""
+                    srcset=""
+                  />
+                </div>
+                <div className="image-of-the-product-details">
+                  <img
+                    style={{
+                      height: "190px",
+                      width: "146px",
+                    }}
+                    src="https://waggfoods.com/assets/images/products/_productImage1xWebp/137/wagg_tasty_bones_150g.webp"
+                    alt=""
+                    srcset=""
+                  />
+                </div>
               </div>
-              <div className="image-of-the-product-details">
-                <img
-                  style={{
-                    height: "190px",
-                    width: "146px",
-                  }}
-                  src="https://waggfoods.com/assets/images/products/_productImage1xWebp/137/wagg_tasty_bones_150g.webp"
-                  alt=""
-                  srcset=""
-                />
-              </div>
-            </div>
-            <div className="bottom-section-about-products-details">
-              <div className="product-name-for-selection">
-                <p>Tasty Bones</p>
-              </div>
-              <div className="sizes-for-all-dogs-selection">
-                <p>All Dog Size</p>
-              </div>
-              <div className="review-price-and-add-to-card-button">
-                <p>
-                  <span>$60.00</span>
-                  <span className="star-review-for-product">
-                    &#11088;{" "}
-                  </span> 4.9{" "}
-                  <span style={{ color: "gray" }}>(8000 reviews)</span>
-                </p>
-                <div className="container-image-for-add-to-cart">
-                  <button className="image-for-add-to-cart" type="submit">
-                    <img
-                      style={{ height: "29px", width: "25px" }}
-                      src="https://cdn-icons-png.flaticon.com/128/4329/4329263.png"
-                      alt=""
-                      srcset=""
-                    />
-                  </button>
+              <div className="bottom-section-about-products-details">
+                <div className="product-name-for-selection">
+                  <p>Tasty Bones</p>
+                </div>
+                <div className="sizes-for-all-dogs-selection">
+                  <p>All Dog Size</p>
+                </div>
+                <div className="review-price-and-add-to-card-button">
+                  <p>
+                    <span>$60.00</span>
+                    <span className="star-review-for-product">
+                      &#11088;{" "}
+                    </span> 4.9{" "}
+                    <span style={{ color: "gray" }}>(8000 reviews)</span>
+                  </p>
+                  <div className="container-image-for-add-to-cart">
+                    <button className="image-for-add-to-cart" type="submit">
+                      <img
+                        style={{ height: "29px", width: "25px" }}
+                        src="https://cdn-icons-png.flaticon.com/128/4329/4329263.png"
+                        alt=""
+                        srcset=""
+                      />
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="shopping-product-cards-container">
-            <div className="product-info-card-for-selection">
-              <div className="image-for-like-the-product">
-                <img
-                  style={{
-                    height: "13px",
-                    width: "13px",
-                  }}
-                  src="https://cdn-icons-png.flaticon.com/128/1077/1077035.png"
-                  alt=""
-                  srcset=""
-                />
+            <div className="shopping-product-cards-container">
+              <div className="product-info-card-for-selection">
+                <div className="image-for-like-the-product">
+                  <img
+                    style={{
+                      height: "13px",
+                      width: "13px",
+                    }}
+                    src="https://cdn-icons-png.flaticon.com/128/1077/1077035.png"
+                    alt=""
+                    srcset=""
+                  />
+                </div>
+                <div className="image-of-the-product-details">
+                  <img
+                    style={{
+                      height: "190px",
+                      width: "146px",
+                    }}
+                    src="https://waggfoods.com/assets/images/products/_productImage1xWebp/137/wagg_tasty_bones_150g.webp"
+                    alt=""
+                    srcset=""
+                  />
+                </div>
               </div>
-              <div className="image-of-the-product-details">
-                <img
-                  style={{
-                    height: "190px",
-                    width: "146px",
-                  }}
-                  src="https://waggfoods.com/assets/images/products/_productImage1xWebp/137/wagg_tasty_bones_150g.webp"
-                  alt=""
-                  srcset=""
-                />
-              </div>
-            </div>
-            <div className="bottom-section-about-products-details">
-              <div className="product-name-for-selection">
-                <p>Tasty Bones</p>
-              </div>
-              <div className="sizes-for-all-dogs-selection">
-                <p>All Dog Size</p>
-              </div>
-              <div className="review-price-and-add-to-card-button">
-                <p>
-                  <span>$60.00</span>
-                  <span className="star-review-for-product">
-                    &#11088;{" "}
-                  </span> 4.9{" "}
-                  <span style={{ color: "gray" }}>(8000 reviews)</span>
-                </p>
-                <div className="container-image-for-add-to-cart">
-                  <button className="image-for-add-to-cart" type="submit">
-                    <img
-                      style={{ height: "29px", width: "25px" }}
-                      src="https://cdn-icons-png.flaticon.com/128/4329/4329263.png"
-                      alt=""
-                      srcset=""
-                    />
-                  </button>
+              <div className="bottom-section-about-products-details">
+                <div className="product-name-for-selection">
+                  <p>Tasty Bones</p>
+                </div>
+                <div className="sizes-for-all-dogs-selection">
+                  <p>All Dog Size</p>
+                </div>
+                <div className="review-price-and-add-to-card-button">
+                  <p>
+                    <span>$60.00</span>
+                    <span className="star-review-for-product">
+                      &#11088;{" "}
+                    </span> 4.9{" "}
+                    <span style={{ color: "gray" }}>(8000 reviews)</span>
+                  </p>
+                  <div className="container-image-for-add-to-cart">
+                    <button className="image-for-add-to-cart" type="submit">
+                      <img
+                        style={{ height: "29px", width: "25px" }}
+                        src="https://cdn-icons-png.flaticon.com/128/4329/4329263.png"
+                        alt=""
+                        srcset=""
+                      />
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
--- src/Containers/FeaturedIn/FeaturedIn.js
+++ src/Containers/FeaturedIn/FeaturedIn.js
@@ -4,49 +4,51 @@ const FeaturedIn = () => {
   return (
     <>
       <div className="main-flex-container-contain-featured-in-icons">
-        <div className="featured-in-header-text">
-          <p style={{ fontFamily: "Acumin Pro" }}>Featured In</p>
-        </div>
-        <div className="flex-featured-in-icons">
-          <div>
-            <img
-              //   style={{ height: "120px", width: "230px" }}
-              src="https://www.cargill.com/image/1432079742021/cargill-logo-black.jpg"
-              alt=""
-              srcset=""
-            />
+        <div className="featuredin-width-container">
+          <div className="featured-in-header-text">
+            <p style={{ fontFamily: "Acumin Pro" }}>Featured In</p>
           </div>
-          <div>
-            <img
-              //   style={{ height: "120px", width: "230px" }}
-              src="https://www.cargill.com/image/1432079742021/cargill-logo-black.jpg"
-              alt=""
-              srcset=""
-            />
-          </div>
-          <div>
-            <img
-              //   style={{ height: "120px", width: "230px" }}
-              src="https://www.cargill.com/image/1432079742021/cargill-logo-black.jpg"
-              alt=""
-              srcset=""
-            />
-          </div>
-          <div>
-            <img
-              //   style={{ height: "120px", width: "230px" }}
-              src="https://www.cargill.com/image/1432079742021/cargill-logo-black.jpg"
-              alt=""
-              srcset=""
-            />
-          </div>
-          <div>
-            <img
-              //   style={{ height: "120px", width: "230px" }}
-              src="https://www.cargill.com/image/1432079742021/cargill-logo-black.jpg"
-              alt=""
-              srcset=""
-            />
+          <div className="flex-featured-in-icons">
+            <div>
+              <img
+                //   style={{ height: "120px", width: "230px" }}
+                src="https://www.cargill.com/image/1432079742021/cargill-logo-black.jpg"
+                alt=""
+                srcset=""
+              />
+            </div>
+            <div>
+              <img
+                //   style={{ height: "120px", width: "230px" }}
+                src="https://www.cargill.com/image/1432079742021/cargill-logo-black.jpg"
+                alt=""
+                srcset=""
+              />
+            </div>
+            <div>
+              <img
+                //   style={{ height: "120px", width: "230px" }}
+                src="https://www.cargill.com/image/1432079742021/cargill-logo-black.jpg"
+                alt=""
+                srcset=""
+              />
+            </div>
+            <div>
+              <img
+                //   style={{ height: "120px", width: "230px" }}
+                src="https://www.cargill.com/image/1432079742021/cargill-logo-black.jpg"
+                alt=""
+                srcset=""
+              />
+            </div>
+            <div>
+              <img
+                //   style={{ height: "120px", width: "230px" }}
+                src="https://www.cargill.com/image/1432079742021/cargill-logo-black.jpg"
+                alt=""
+                srcset=""
+              />
+            </div>
           </div>
         </div>
       </div>
