4ad90d5321c502b824af74d3c26efd8dea62cec0
Prasad Adhav <prasadadhav22@gmail.com>
2022-08-13 12:27:03 +0530

    pull for updated code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import "./App.css";
 import ResponsiveNav from "./Components/ResponsiveNav/ResponsiveNav";
-// import FeaturedIn from "./Containers/FeaturedIn/FeaturedIn";
-// import TargetSale from "./Containers/TargetSales/TargetSale";
-// import HomeShopNow from "./Components/HomeShopNow/HomeShopNow";
-
-// import ExploreWorld from "./Containers/ExploreWorld/ExploreWorld";
-// import UkStore from "./Components/UkStore/UkStore";
-// import ProductNav from "./Components/ProductNav/ProductNav";
-// import ReviewRewards from "./Components/ReviewRewards/ReviewRewards";
-// import PetShopFooter from "./Components/PetShopFooter/PetShopFooter";
-// import ShopOurInstagram from "./Containers/ShopOurInstagram/ShopOurInstagram";
-// import ExploreMore from "./Containers/ExploreMore/ExploreMore";
-// import ProductPage from "./Containers/ProductPage/ProductPage";
-// import Nav from "./Components/Nav/Nav";
-// import AboutUs from "./Containers/AboutUs/AboutUs";
-// import ResponsiveNav from "./Components/ResponsiveNav/ResponsiveNav";
+import AddAddress from "./Containers/AddAddress/AddAddress";
+import ShowAddress from "./Containers/AddAddress/showAddress";
 import Home from "./Containers/Home";
 import PreviewTheme from "./Containers/PreviewTheme/PreviewTheme";
 
 function App() {
   return (
     <>
-      <PreviewTheme />
+      <ShowAddress />
+      <AddAddress />
+      {/* <PreviewTheme /> */}
     </>
   );
 }
