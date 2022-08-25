8df4103493d533bc767e50a08ac5d10dd5a09455
Prasad Adhav <prasadadhav22@gmail.com>
2022-08-25 16:02:24 +0530

    Updated Code

--- src/Components/CaseStudyComponent/CaseStudyComponent.js
+++ src/Components/CaseStudyComponent/CaseStudyComponent.js
@@ -6,17 +6,17 @@ const CaseStudyComponent = (props) => {
     <>
       {/* Component for case study that we are using in CaseStudyContainer */}
       {/* flex div for all content */}
-      <div className="flex-div-container-for-case-study">
-        {/* width set for component */}
-        <div className="width-set-case-study-component">
-          <div className="information-for-component">
-            <h3>{props.CSSDetail.title} BEST CHICKEN & MEATS</h3>
-            <h1>{props.CSSDetail.heading}</h1>
-            <p>{props.CSSDetail.info}</p>
-            <button type="submit">Show More</button>
-          </div>
-        </div>
+      {/* <div className="flex-div-container-for-case-study"> */}
+      {/* width set for component */}
+      {/* <div className="width-set-case-study-component"> */}
+      <div className="information-for-component">
+        <h3>{props.title}</h3>
+        <h1>{props.heading}</h1>
+        <p>{props.info}</p>
+        <button type="submit">Show More</button>
       </div>
+      {/* </div> */}
+      {/* </div> */}
     </>
   );
 };
--- src/Containers/CaseStudyScreen/CaseStudyScreen.js
+++ src/Containers/CaseStudyScreen/CaseStudyScreen.js
@@ -4,23 +4,6 @@ import CaseStudyComponent from "./../../Components/CaseStudyComponent/CaseStudyC
 // import bgimg from "../../Assets/CaseStudyComponent/CaseStudyImg-1.jpg";
 
 const CaseStudyScreen = () => {
-  // const cssdata = [
-  //   {
-  //     _id: "1",
-  //     title: "BEST CHICKEN & MEATS",
-  //     heading: "Always the Best.",
-  //     info: "Brand Positioning, Design and Store Styling for a new chain of meat stores in India from Indonesian multinational JAPFA.",
-  //     url: bgimg,
-  //   },
-  //   {
-  //     _id: "1",
-  //     title: "BEST CHICKEN & MEATS",
-  //     heading: "Always the Best.",
-  //     info: "Brand Positioning, Design and Store Styling for a new chain of meat stores in India from Indonesian multinational JAPFA.",
-  //     url: bgimg,
-  //   },
-
-  // ];
   return (
     <>
       {/* contains case study components */}
@@ -33,17 +16,69 @@ const CaseStudyScreen = () => {
             <h1>Our Work</h1>
           </div>
           {/* div for Case Study component */}
-
-          <div className="div-contains-all-components-case-study">
-            <div className="content-for-the-img">
-              {/* {cssdata.map((val, index) => (
-                <CaseStudyComponent CSSDetail={val} />
-              ))} */}
-              <CaseStudyComponent
-                title="BEST CHICKEN & MEATS"
-                heading="Always the Best."
-                info="Brand Positioning, Design and Store Styling for a new chain of meat stores in India from Indonesian multinational JAPFA."
-              />
+          <div className="section-for-all-content">
+            <div className="div-contains-all-components-case-study-1">
+              <div className="content-for-the-img-1">
+                <CaseStudyComponent
+                  title="BEST CHICKEN & MEATS"
+                  heading="Always the Best."
+                  info="Brand Positioning, Design and Store Styling for a new chain of meat stores in India from Indonesian multinational JAPFA."
+                />
+              </div>
+            </div>
+            <div className="div-contains-all-components-case-study-2">
+              <div className="content-for-the-img-2">
+                <CaseStudyComponent
+                  title="BEST CHICKEN & MEATS"
+                  heading="Always the Best."
+                  info="Brand Positioning, Design and Store Styling for a new chain of meat stores in India from Indonesian multinational JAPFA."
+                />
+              </div>
+            </div>
+            <div className="div-contains-all-components-case-study-3">
+              <div className="content-for-the-img-3">
+                <CaseStudyComponent
+                  title="BEST CHICKEN & MEATS"
+                  heading="Always the Best."
+                  info="Brand Positioning, Design and Store Styling for a new chain of meat stores in India from Indonesian multinational JAPFA."
+                />
+              </div>
+            </div>
+            <div className="div-contains-all-components-case-study-4">
+              <div className="content-for-the-img-4">
+                <CaseStudyComponent
+                  title="BEST CHICKEN & MEATS"
+                  heading="Always the Best."
+                  info="Brand Positioning, Design and Store Styling for a new chain of meat stores in India from Indonesian multinational JAPFA."
+                />
+              </div>
+            </div>
+            <div className="div-contains-all-components-case-study-5">
+              <div className="content-for-the-img-5">
+                <CaseStudyComponent
+                  title="BEST CHICKEN & MEATS"
+                  heading="Always the Best."
+                  info="Brand Positioning, Design and Store Styling for a new chain of meat stores in India from Indonesian multinational JAPFA."
+                />
+              </div>
+            </div>
+            <div className="div-contains-all-components-case-study-6">
+              <div className="content-for-the-img-6">
+                <CaseStudyComponent
+                  title="BEST CHICKEN & MEATS"
+                  heading="Always the Best."
+                  info="Brand Positioning, Design and Store Styling for a new chain of meat stores in India from Indonesian multinational JAPFA."
+                />
+              </div>
+            </div>
+            <div className="div-contains-all-components-case-study-7">
+              <div className="content-for-the-img-7">
+                <CaseStudyComponent
+                  title="BEST CHICKEN & MEATS"
+                  heading="Always the Best."
+                  info="Brand Positioning, Design and Store Styling for a new chain of meat stores in India from Indonesian multinational JAPFA."
+                />
+              </div>
             </div>
           </div>
         </div>
